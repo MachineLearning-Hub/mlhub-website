052fca561c476ea6a99ea16f86c432ed5caa7b60
Gurpreet Deol <deol.gs@hotmail.com>
2025-01-03 13:00:09 -0800

    Updated the index file

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('index.html')
 
 
