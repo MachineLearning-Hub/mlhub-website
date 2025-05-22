91324df29a1e2ff40d3f1b544a545e4accafcae7
Gurpreet Deol <deol.gs@hotmail.com>
2025-05-22 12:07:23 -0700

    Updated MLHub home page

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
